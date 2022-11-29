04893ee50bb6e0dd9c185f0df5b4b8fbbca9f5b0
Chineze Ogugua <christabelogugua@gmail.com>
2022-11-29 00:42:05 -0800

    fixing base model

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,8 @@
-#!/usr/bin/python3
-"""Defines the Amenity class."""
+#!/usr/bin/python
+""" class Amenity"""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Represent an amenity.
-    Attributes:
-        name (str): The name of the amenity.
-    """
-
+    """Representation of Amenity"""
     name = ""
